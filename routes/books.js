420e98259f7ef8d8cb1fc798922670415183b34a
yannick andres <yannick.andres@swisscom.com>
2023-08-28 09:57:51 +0200

    feat: requests adapted to the changes in the database

--- routes/books.js
+++ routes/books.js
@@ -5,15 +5,27 @@ const router = express.Router();
 
 //All routes automaticly start with "books"
 router.get('/', (req, res) => {
-  connection.query('SELECT * FROM Books', (err, results) => {
+  const query = `
+    SELECT Books.Id, Books.Titel, Books.Erscheinungsjahr, Autor.FullName as Autor 
+    FROM Books 
+    JOIN Autor ON Books.AutorID = Autor.ID
+  `;
+  connection.query(query, (err, results) => {
     if (err) throw err;
     res.json(results);
   });
 });
 
+
 router.get('/:id', (req, res) => {
   const id = req.params.id;
-  connection.query('SELECT * FROM Books WHERE Id = ?', [id], (err, results) => {
+  const query = `
+    SELECT Books.Id, Books.Titel, Books.Erscheinungsjahr, Autor.FullName as Autor 
+    FROM Books 
+    JOIN Autor ON Books.AutorID = Autor.ID
+    WHERE Books.Id = ?
+  `;
+  connection.query(query, [id], (err, results) => {
     if (err) throw err;
     if (results.length > 0) {
       res.json(results[0]);
@@ -23,44 +35,69 @@ router.get('/:id', (req, res) => {
   });
 });
 
+
 router.post('/', (req, res) => {
   const { Titel, Erscheinungsjahr, Autor } = req.body;
 
   if (!Titel || !Erscheinungsjahr || !Autor) {
-    return res.status(400).send('Title, year of publication and author are required');
+    return res.status(400).send('Title, year of publication, and author are required');
   }
-
-  const newBook = { Titel, Erscheinungsjahr, Autor };
-
-  connection.query('INSERT INTO Books SET ?', newBook, (err) => {
+  connection.query('SELECT ID FROM Autor WHERE FullName = ?', [Autor], (err, results) => {
     if (err) throw err;
-    res.status(201).send('Book successfully created');
+
+    let AutorID;
+    if (results.length > 0) {
+      AutorID = results[0].ID;
+    } else {
+      connection.query('INSERT INTO Autor (FullName) VALUES (?)', [Autor], (err, results) => {
+        if (err) throw err;
+        AutorID = results.insertId;
+      });
+    }
+
+    const newBook = { Titel, Erscheinungsjahr, AutorID };
+
+    connection.query('INSERT INTO Books SET ?', newBook, (err) => {
+      if (err) throw err;
+      res.status(201).send('Book successfully created');
+    });
   });
 });
+
 
 router.put('/:id', (req, res) => {
   const bookId = req.params.id;
   const { Titel, Erscheinungsjahr, Autor } = req.body;
 
   if (!Titel || !Erscheinungsjahr || !Autor) {
-    return res.status(400).send("Title, year of publication and author are required");
+    return res.status(400).send("Title, year of publication, and author are required");
   }
-
-  connection.query('SELECT * FROM Books WHERE Id = ?', [bookId], (err, results) => {
+  connection.query('SELECT ID FROM Autor WHERE FullName = ?', [Autor], (err, results) => {
     if (err) throw err;
 
+    let AutorID;
     if (results.length > 0) {
-      const query = `UPDATE Books SET Titel = ?, Erscheinungsjahr = ?, Autor = ? WHERE Id = ?`;
-
-      connection.query(query, [Titel, Erscheinungsjahr, Autor, bookId], (err) => {
-        if (err) throw err;
-        res.send("Book successfully updated");
-      });
+      AutorID = results[0].ID;
     } else {
-      res.status(404).send("Book not found");
+      connection.query('INSERT INTO Autor (FullName) VALUES (?)', [Autor], (err, results) => {
+        if (err) throw err;
+        AutorID = results.insertId;
+      });
     }
+
+    const query = `
+      UPDATE Books 
+      SET Titel = ?, Erscheinungsjahr = ?, AutorID = ? 
+      WHERE Id = ?
+    `;
+
+    connection.query(query, [Titel, Erscheinungsjahr, AutorID, bookId], (err) => {
+      if (err) throw err;
+      res.send("Book successfully updated");
+    });
   });
 });
+
 
 
 router.delete('/:id', (req, res) => {
@@ -81,7 +118,6 @@ router.delete('/:id', (req, res) => {
     }
   });
 });
-
 
 
 
